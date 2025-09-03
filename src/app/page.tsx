fb59188db110f88e5f95048c072ad93cd27073a7
Val-d-emar <vhot@live.ru>
2025-09-04 00:33:36 +0400

    feat: setup log  wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { err, log, warn } from '@/log';
+
 export default function Home() {
+  log('test logging');
+  err('test error');
+  warn('test warning');
+
   return (
     <main>
       <h1>REST Client App</h1>
