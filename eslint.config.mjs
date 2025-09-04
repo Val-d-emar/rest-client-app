a5a468a0d31c61a1702e88de68d51ac71343be7d
Val-d-emar <vhot@live.ru>
2025-09-04 13:46:17 +0400

    fix: lint & type check tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals'),
   {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      ,
+      'coverage/**',
+    ],
   },
   prettierConfig,
 ];
