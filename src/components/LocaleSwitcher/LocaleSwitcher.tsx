7e427da95c3d58c247f9bddb7244b4fdb6e68240
Anna Perevedentseva <anna.pereved@gmail.com>
2025-09-04 22:31:40 +0200

    feat: implement language switching

--- src/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,17 +1,39 @@
+'use client';
+
+import { Locale, useLocale, useTranslations } from 'next-intl';
+import { useParams } from 'next/navigation';
+import { ChangeEvent, useTransition } from 'react';
+
+import { usePathname, useRouter } from '@/i18n/navigation';
 import { routing } from '@/i18n/routing';
-import { useLocale, useTranslations } from 'next-intl';
 
 export default function LocaleSwitcher() {
   const t = useTranslations('LocaleSwitcher');
-  const locale = useLocale();
+  const defaultValue = useLocale();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const pathname = usePathname();
+  const params = useParams();
+
+  function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
+    const nextLocale = event.target.value as Locale;
+    startTransition(() => {
+      router.replace({ pathname, params } as Parameters<typeof router.replace>[0], {
+        locale: nextLocale,
+      });
+    });
+  }
 
   return (
-    <select>
-      {routing.locales.map((locale) => (
-        <option key={locale} value={locale}>
-          {t(locale)}
-        </option>
-      ))}
-    </select>
+    <label>
+      <p className='sr-only'>{t('label')}</p>
+      <select defaultValue={defaultValue} disabled={isPending} onChange={onSelectChange}>
+        {routing.locales.map((locale) => (
+          <option key={locale} value={locale}>
+            {t(locale)}
+          </option>
+        ))}
+      </select>
+    </label>
   );
 }
