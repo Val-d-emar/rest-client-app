4bc03379ca8f4da858b69d1a90b22211f9657613
Anna Perevedentseva <anna.pereved@gmail.com>
2025-09-05 19:37:47 +0200

    feat: add logo to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@
 import LocaleSwitcher from '@/components/LocaleSwitcher';
 import { useTranslations } from 'next-intl';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 import { Link } from '@/i18n/navigation';
 
@@ -21,12 +22,13 @@ export default function Header() {
 
   return (
     <header className={`${classes.header} ${scrolled ? classes.scrolled : ''}`}>
-      <div className={`container ${classes['flex-wrapper']}`}>
-        <Link href='/'>
-          <h1>{t('title')}</h1>
+      <nav className={`container ${classes['flex-wrapper']}`}>
+        <Link href='/' className={classes['logo-wrapper']}>
+          <Image src='/logo.png' alt={t('logoAlt')} width={60} height={60} />
+          <span className={classes.brandText}>{t('title')}</span>
         </Link>
 
-        <nav className={classes.controls}>
+        <div className={classes.controls}>
           <LocaleSwitcher />
           {/* TODO: отображать кнопки в зависимости от состояния юзера */}
           <Link href='/auth/signin' className={classes.button}>
@@ -36,8 +38,8 @@ export default function Header() {
             {t('SignUpLabel')}
           </Link>
           {/* <Link href='/'>{t('SignOutLabel')}</Link> */}
-        </nav>
-      </div>
+        </div>
+      </nav>
     </header>
   );
 }
