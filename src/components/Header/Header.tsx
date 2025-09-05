b4ca0f8a96ddc6c1e9e5a3a90ef7e8470b35254c
Anna Perevedentseva <anna.pereved@gmail.com>
2025-09-05 17:21:44 +0200

    feat: implement sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,28 @@
+'use client';
+
 import LocaleSwitcher from '@/components/LocaleSwitcher';
 import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
+
+import classes from './Header.module.css';
 
 export default function Header() {
   const t = useTranslations('HomePage');
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => setScrolled(window.scrollY > 0);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <header>
-      <h1>{t('title')}</h1>
-      <LocaleSwitcher />
+    <header className={`${classes.header} ${scrolled ? classes.scrolled : ''}`}>
+      <div className={`container ${classes['flex-wrapper']}`}>
+        <h1>{t('title')}</h1>
+        <LocaleSwitcher />
+      </div>
     </header>
   );
 }
