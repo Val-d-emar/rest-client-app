a489c25cd91a636bccd7d24b2a0ebb9a62b7b422
Anna Perevedentseva <anna.pereved@gmail.com>
2025-09-08 01:18:42 +0200

    refactor: improve table styles

--- src/components/RequestHeaders/RequestHeaders.tsx
+++ src/components/RequestHeaders/RequestHeaders.tsx
@@ -75,69 +75,35 @@ export default function RequestHeaders() {
   return (
     <>
       <h3>HTTP headers</h3>
-      <table>
-        <tbody>
-          {headers.map((header) => (
-            <tr key={header.id}>
-              <td>
-                <input
-                  type='checkbox'
-                  checked={header.enabled}
-                  className={classes.checkbox}
-                  onChange={() => onHeaderEnable(header.id)}
-                />
-              </td>
-              <td>
-                <input
-                  type='text'
-                  value={header.key}
-                  onChange={(e) => onHeaderInputChange(header.id, e.target.value, 'key')}
-                  placeholder={t('headerKeyPlaceholder')}
-                />
-              </td>
-              <td>
-                <input
-                  type='text'
-                  value={header.value}
-                  onChange={(e) => onHeaderInputChange(header.id, e.target.value, 'value')}
-                  placeholder={t('headerValuePlaceholder')}
-                />
-              </td>
-              <td>
-                <button
-                  type='button'
-                  className={classes.button}
-                  onClick={() => onRemove(header.id)}
-                >
-                  <svg
-                    width='20px'
-                    height='20px'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <g clipPath='url(#clip0_429_11083)'>
-                      <path
-                        d='M7 7.00006L17 17.0001M7 17.0001L17 7.00006'
-                        stroke='currentColor'
-                        strokeWidth='2.5'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id='clip0_429_11083'>
-                        <rect width='24' height='24' fill='white' />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </button>
-              </td>
-            </tr>
-          ))}
-          <tr>
-            <td>
-              <button type='button' className={classes.button} onClick={onAddClick}>
+      <div className={classes.table}>
+        {headers.map((header) => (
+          <div key={header.id} className={classes.row}>
+            <div>
+              <input
+                type='checkbox'
+                checked={header.enabled}
+                className={classes.checkbox}
+                onChange={() => onHeaderEnable(header.id)}
+              />
+            </div>
+            <div>
+              <input
+                type='text'
+                value={header.key}
+                onChange={(e) => onHeaderInputChange(header.id, e.target.value, 'key')}
+                placeholder={t('headerKeyPlaceholder')}
+              />
+            </div>
+            <div>
+              <input
+                type='text'
+                value={header.value}
+                onChange={(e) => onHeaderInputChange(header.id, e.target.value, 'value')}
+                placeholder={t('headerValuePlaceholder')}
+              />
+            </div>
+            <div>
+              <button type='button' className={classes.button} onClick={() => onRemove(header.id)}>
                 <svg
                   width='20px'
                   height='20px'
@@ -145,19 +111,47 @@ export default function RequestHeaders() {
                   fill='none'
                   xmlns='http://www.w3.org/2000/svg'
                 >
-                  <path
-                    d='M6 12H18M12 6V18'
-                    stroke='currentColor'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                  />
+                  <g clipPath='url(#clip0_429_11083)'>
+                    <path
+                      d='M7 7.00006L17 17.0001M7 17.0001L17 7.00006'
+                      stroke='currentColor'
+                      strokeWidth='2.5'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id='clip0_429_11083'>
+                      <rect width='24' height='24' fill='white' />
+                    </clipPath>
+                  </defs>
                 </svg>
               </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            </div>
+          </div>
+        ))}
+        <div>
+          <div>
+            <button type='button' className={classes.button} onClick={onAddClick}>
+              <svg
+                width='20px'
+                height='20px'
+                viewBox='0 0 24 24'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M6 12H18M12 6V18'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
