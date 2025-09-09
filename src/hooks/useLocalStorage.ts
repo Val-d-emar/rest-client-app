b16bc1b470008afc80c814342602e0197a5530a5
Val-d-emar <vhot@live.ru>
2025-09-09 20:41:49 +0400

    fix: eslint warning

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,41 +1,36 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { dbg, warn } from '@/log';
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
-  const readValue = (): T => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
     if (typeof window === 'undefined') {
       return initialValue;
     }
-
     try {
       const item = window.localStorage.getItem(key);
-      return item ? (JSON.parse(item) as T) : initialValue;
+      return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       dbg(`Error reading localStorage key “${key}”:`, error);
       return initialValue;
     }
-  };
+  });
 
-  const [storedValue, setStoredValue] = useState<T>(readValue);
-
-  const setValue = (value: T) => {
-    if (typeof window === 'undefined') {
-      warn(`Tried setting localStorage key “${key}” even though environment is not a client`);
-    }
-
-    try {
-      window.localStorage.setItem(key, JSON.stringify(value));
-      setStoredValue(value);
-    } catch (error) {
-      dbg(`Error setting localStorage key “${key}”:`, error);
-    }
-  };
-
-  useEffect(() => {
-    setStoredValue(readValue());
-  }, []);
+  const setValue = useCallback(
+    (value: T | ((val: T) => T)) => {
+      try {
+        const valueToStore = value instanceof Function ? value(storedValue) : value;
+        setStoredValue(valueToStore);
+        if (typeof window !== 'undefined') {
+          window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        }
+      } catch (error) {
+        dbg(`Error setting localStorage key “${key}”:`, error);
+      }
+    },
+    [key, storedValue],
+  );
 
   return [storedValue, setValue];
 }
