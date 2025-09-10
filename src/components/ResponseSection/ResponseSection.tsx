75f934d55af926bf9e2ef6735d63d31fc6ab0312
Anna Perevedentseva <anna.pereved@gmail.com>
2025-09-10 02:14:22 +0200

    feat: improve headers styles

--- src/components/ResponseSection/ResponseSection.tsx
+++ src/components/ResponseSection/ResponseSection.tsx
@@ -1,6 +1,7 @@
 import { ServerResponse } from '@/lib/actions/request';
 import Spinner from '@/components/Spinner/Spinner';
 import BodyPane from '../RequestBody/BodyPane';
+import ResponseHeaders from './ResponseHeaders';
 
 type ResponseSectionProps = {
   response: ServerResponse | null;
@@ -27,13 +28,6 @@ export default function ResponseSection({ response, loading }: ResponseSectionPr
   return (
     <div>
       <h3>Response</h3>
-      <p>
-        <span className='bold'>Status:</span> {response.status}
-      </p>
-      <h4>Headers:</h4>
-      <pre className='response-viewer response-viewer-headers'>
-        {JSON.stringify(response.headers, null, 2)}
-      </pre>
       <h4>Response body:</h4>
       <BodyPane
         body={JSON.stringify(response.body, null, 2)}
@@ -41,6 +35,8 @@ export default function ResponseSection({ response, loading }: ResponseSectionPr
         status={response.status || undefined}
         statusText={response.statusText || undefined}
       />
+      <h4>Headers:</h4>
+      <ResponseHeaders headers={response.headers} />
     </div>
   );
 }
