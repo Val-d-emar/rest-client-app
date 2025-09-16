5f9a0b1c33a3849be5e0ea24461b55f03edf2b12
Val-d-emar <vhot@live.ru>
2025-09-16 17:23:17 +0400

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from 'next';
+import { execSync } from 'child_process';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
     NEXT_PUBLIC_GIT_COMMIT_SHA:
-      process.env.VERCEL_GIT_COMMIT_SHA ||
-      require('child_process').execSync('git rev-parse HEAD').toString().trim(),
+      process.env.VERCEL_GIT_COMMIT_SHA || execSync('git rev-parse HEAD').toString().trim(),
     NEXT_PUBLIC_BUILD_TIMESTAMP: new Date().toUTCString(),
   },
   compiler: {
