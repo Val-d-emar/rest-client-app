c269223d2a78937e8e44f894cf6fd85c92bc3aaa
Val-d-emar <vhot@live.ru>
2025-09-16 15:17:50 +0400

    feat: add build date && commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_GIT_COMMIT_SHA:
+      process.env.VERCEL_GIT_COMMIT_SHA ||
+      require('child_process').execSync('git rev-parse HEAD').toString().trim(),
+    NEXT_PUBLIC_BUILD_TIMESTAMP: new Date().toUTCString(),
+  },
   compiler: {
     removeConsole: false,
   },
