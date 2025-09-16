8f955ee18204d3da978053d529060687876cd421
Anna Perevedentseva <anna.pereved@gmail.com>
2025-09-16 22:04:10 +0200

    feat: add adaptive styles for variables page

--- src/components/variables/variables-page.tsx
+++ src/components/variables/variables-page.tsx
@@ -4,6 +4,7 @@ import { useAuth } from '@/context/AuthContext';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { useTranslations } from 'next-intl';
 import { v4 as uuidv4 } from 'uuid';
+import classes from './variables-page.module.css';
 
 type VariableItem = {
   id: string;
@@ -33,39 +34,43 @@ export default function VariablesPage() {
   };
 
   return (
-    <section className='container' style={{ maxWidth: '800px' }}>
+    <section className={classes.wrapper}>
       <h1>{t('title')}</h1>
       <p className='centered'>{t('description')}</p>
 
-      <div className='card' style={{ marginTop: '2rem' }}>
-        {variables.map((variable) => (
-          <div key={variable.id} style={{ display: 'flex', gap: '1rem', marginBottom: '1rem' }}>
-            <input
-              type='text'
-              placeholder={t('keyPlaceholder')}
-              value={variable.key}
-              onChange={(e) => handleVariableChange(variable.id, 'key', e.target.value)}
-              style={{ flexGrow: 1 }}
-            />
-            <input
-              type='text'
-              placeholder={t('valuePlaceholder')}
-              value={variable.value}
-              onChange={(e) => handleVariableChange(variable.id, 'value', e.target.value)}
-              style={{ flexGrow: 2 }}
-            />
-            <button
-              type='button'
-              onClick={() => handleRemoveVariable(variable.id)}
-              className='error-button'
-            >
-              {t('deleteButton')}
-            </button>
-          </div>
-        ))}
-        <button type='button' onClick={handleAddVariable}>
-          {t('addButton')}
-        </button>
+      <div className={classes['variables-wrapper']}>
+        <div className='card'>
+          {variables.map((variable) => (
+            <div key={variable.id} className={classes.row}>
+              <div className={classes['inputs-wrapper']}>
+                <input
+                  type='text'
+                  placeholder={t('keyPlaceholder')}
+                  value={variable.key}
+                  onChange={(e) => handleVariableChange(variable.id, 'key', e.target.value)}
+                  style={{ flexGrow: 1 }}
+                />
+                <input
+                  type='text'
+                  placeholder={t('valuePlaceholder')}
+                  value={variable.value}
+                  onChange={(e) => handleVariableChange(variable.id, 'value', e.target.value)}
+                  style={{ flexGrow: 2 }}
+                />
+              </div>
+              <button
+                type='button'
+                onClick={() => handleRemoveVariable(variable.id)}
+                className={`error-button ${classes.button}`}
+              >
+                {t('deleteButton')}
+              </button>
+            </div>
+          ))}
+          <button type='button' onClick={handleAddVariable} className={classes.button}>
+            {t('addButton')}
+          </button>
+        </div>
       </div>
     </section>
   );
